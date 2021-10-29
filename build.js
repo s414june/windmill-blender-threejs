5e930951c909019ae94d010632b20e90c6d95cae
雨芩 吳 <48972804+s414june@users.noreply.github.com>
2021-10-29 16:17:29 +0800

    m

--- build.js
+++
@@ -1,8 +0,0 @@
-var copy = require('recursive-copy');
-
-async function init() {
-  await copy('src', 'dist');
-  await copy('build/contracts', 'dist');
-}
-
-init();
--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -4,22 +4,22 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/script.js'),
+    entry: path.resolve(__dirname, './../src/script.js'),
     output:
     {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, '../dist')
+        filename: './bundle.[contenthash].js',
+        path: path.resolve(__dirname, './../dist')
     },
     devtool: 'source-map',
     plugins:
     [
         new CopyWebpackPlugin({
             patterns: [
-                { from: path.resolve(__dirname, '../static') }
+                { from: path.resolve(__dirname, './../static') }
             ]
         }),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
+            template: path.resolve(__dirname, './../src/index.html'),
             minify: true
         }),
         new MiniCSSExtractPlugin()
